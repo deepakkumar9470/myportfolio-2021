61777225c234c5e316ab0a431d7ee7cc62371eb1
deepakkumar9470 <dk397787@gmail.com>
2021-09-09 10:58:55 +0530

    myportfolio2021

---
+++ Js/info.js
@@ -0,0 +1,44 @@
+/*For Arrow Buton
+
+let mybutton=document.getElementById("mybtn");
+window.onscroll= function() {scrollFunction()};
+function scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display="block";
+
+    }else{
+        mybutton.style.display="none";
+    }
+    
+}
+
+function topfunction(){
+    document.body.scrollTop = 0; // For safari
+    document.documentElement.scrollTop = 0; // for chrome , firefox and others
+}*/
+
+//$(document).ready(function(){
+//$('.project-section .navigation .button #btn1').trigger('click');
+//
+//$('.project-section .image-container .test-popup-link').magnificPopup({
+//   type:'image',
+//   gallery:{enabled:true}
+//});
+//    
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
